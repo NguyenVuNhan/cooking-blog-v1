6778ef6893c7fcf9d2b8fd30caa0a910b26ae06f
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2020-08-08 09:49:08 +0700

    Test running on heroku

---
+++ config/keys.dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoUrl:
+		"mongodb+srv://nvn:EytihV2nPU4iALjU@cluster0.jbhpw.mongodb.net/cooking-blog?retryWrites=true&w=majority",
+	secretOrKey: "ucUGnuuhfCuxfUPTRYpSBmAlGqCmK4s0"
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
+
+const app = express();
+
+// Body-parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Passport Middleware
+// app.use(passport.initialize());
+// require("./config/passport")(passport);
+
+// Database conection
+const db = require("./config/keys").mongoUrl;
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log("Database Connected"))
+	.catch(err => console.log(err));
+
+// Routes
+app.get("/test", (req, res) => {
+	res.send("Cooking Blog test");
+});
+
+// Server static
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
