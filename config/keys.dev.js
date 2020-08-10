9ea3624183582b6721c4ab9ca3e2969ae825a5a6
NguyenVuNhan <nguyenvunhan1999@gmail.com>
2020-08-10 22:03:22 +0700

    Clean up

--- config/keys.dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	mongoUrl:
-		"mongodb+srv://nvn:EytihV2nPU4iALjU@cluster0.jbhpw.mongodb.net/cooking-blog?retryWrites=true&w=majority",
-	secretOrKey: "ucUGnuuhfCuxfUPTRYpSBmAlGqCmK4s0"
-};
